5d5d946990847c75ac07e25fc4a26f0fc8700b7b
waseemkh88 <124219085+waseemkh88@users.noreply.github.com>
2023-06-12 12:46:30 +0300

    add ownership control and acls to the source buckets

--- s3.tf
+++ s3.tf
@@ -1,18 +1,71 @@
+##### artifacts_bucket #####
+
 data aws_s3_bucket codepipeline_bucket{
-  name = local.artifacts_bucket_name
+  bucket = local.artifacts_bucket_name
 }
 
-resource "aws_s3_bucket_ownership_controls" "source_codebuild_bucket" {
+resource "aws_s3_bucket_ownership_controls" "codepipeline_bucket_oc" {
   bucket = data.aws_s3_bucket.codepipeline_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_acl" "source_codebuild_bucket" {
+resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
   bucket = data.aws_s3_bucket.codepipeline_bucket.id
   acl    = "private"
-  depends_on = [
-    aws_s3_bucket.codepipeline_bucket, aws_s3_bucket_versioning.codepipeline_bucket,aws_s3_bucket_ownership_controls.source_codebuild_bucket
-  ]
+}
+
+##### target_bucket #####
+
+data aws_s3_bucket target_bucket{
+  id = var.target_bucket
+}
+
+resource "aws_s3_bucket_ownership_controls" "target_bucket_oc" {
+  bucket = data.aws_s3_bucket.target_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "target_bucket_acl" {
+  bucket = data.aws_s3_bucket.target_bucket.id
+  acl    = "private"
+}
+
+##### src_bucket #####
+
+data aws_s3_bucket src_bucket{
+  id = var.src_bucket
+}
+
+resource "aws_s3_bucket_ownership_controls" "src_bucket_oc" {
+  bucket = data.aws_s3_bucket.src_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "src_bucket_acl" {
+  bucket = data.aws_s3_bucket.src_bucket.id
+  acl    = "private"
+}
+
+##### test_bucket #####
+
+data aws_s3_bucket test_bucket{
+  id = var.test_bucket
+}
+
+resource "aws_s3_bucket_ownership_controls" "test_bucket_oc" {
+  bucket = data.aws_s3_bucket.test_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test_bucket_acl" {
+  bucket = data.aws_s3_bucket.test_bucket.id
+  acl    = "private"
 }
